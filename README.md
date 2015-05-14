29ce89d6bde12f2d7ef14506c22ea96b45de75d9
marcos_navarro <MN@MARCOSs-MBP.lan>
2015-05-13 20:11:44 -0600

    Start with collections, fixtures

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Meteor app example.
 - Add bootstrap (meteor add twbs:bootstrap)
 - Add stylus (meteor add stylus)
 - Create client > templates > first templates
+
+## 2.0 Start with Collections
+- Create collections folder
+- Declare posts collection
+- More elaborated posts data
+- Add (meteor add fortawesome:fontawesome)
+- Update post item template
+- Create posts fixture for DB
+- Run meteor on a known db (MONGO_URL=mongodb://localhost:27017/memeapp meteor)
+- See posts in console (Posts.find().fetch();)
+- Update helpers for posts list
+- Remove autopublish, insecure
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,32 +1,31 @@
-var postsData = [
-	{
+// If theres no posts
+if (Posts.find().count() === 0) {
+	Posts.insert({
 		publisher: 'Bobby Dobby',
 		date: '1 hour ago',
 		title: 'Yo solo digo...',
 		src: 'http://cdn.vuelodigital.com/wp-content/uploads/2014/07/meme-osea.jpg',
 		comment: 'A comment here...',
 		stars: 4
-	}, 
-	{
+	});
+
+	Posts.insert({
 		publisher: 'Hugo Tarugo',
 		date: '1 hour ago',
 		title: 'Gryffindor',
 		src: 'http://cdn.smosh.com/sites/default/files/bloguploads/60s-spiderman-meme-gryffindor.jpg',
 		comment: 'A comment here...',
 		stars: 2
-	}, 
-	{
+	});
+
+	Posts.insert({
 		publisher: 'Plastilina Mosh',
 		date: '1 hour ago',
 		title: 'Deal with it',
 		src: 'http://blog.100x100.net/wp-content/uploads/2013/10/obama-wins-reelection-memes.jpg',
 		comment: 'A comment here...',
 		stars: 0
-	}
-];
+	});
 
-Template.postsList.helpers({
-	posts: function(){
-		return Posts.find();
-	}
-});
+	console.log('Created dummy posts!');
+};
